7dc902f5f0ff75a5126f496b9575a55ee7fa379f
Gabriel Ruiz <51479498+GobsRuiz@users.noreply.github.com>
2021-03-01 10:19:37 -0300

    Chat animation when loading the page

--- Hyperspace/js/animation/loadPage.js
+++ Hyperspace/js/animation/loadPage.js
@@ -1,11 +1,19 @@
 function loadPage() {
     window.addEventListener("load", () => {
+        // Header
         let logo = document.getElementById("header_top_logo");
         let languages = document.querySelectorAll("#header_top_languages a");
         let menuLinks = document.querySelectorAll("#header_top_menu a");
         let headerBottomTexth1 = document.querySelector("#header_bottom_text h1");
         let headerBottomTextp = document.querySelector("#header_bottom_text p");
 
+        // Chat
+        let chatIcon = document.getElementById("chat_icon_chat");
+        let chatMessage = document.getElementById("chat_message");
+
+
+
+        // page loaded at the top
         if(window.pageYOffset == 0 || window.pageYOffset < 150){
             // Logo
             defineStyle(logo);
@@ -30,11 +38,21 @@ function loadPage() {
             // Header bottom text p
             defineStyle(headerBottomTextp)
         }
+
+        // Chat
+        setTimeout(() => {
+            defineStyle(chatIcon);
+            
+            setTimeout(() => {
+                defineStyle(chatMessage);
+            }, 300);
+        }, 3000);
     })
 
     // Define style
     function defineStyle(element){
         element.style.opacity = 1;
-        element.style.bottom = 0;
+        element.style.left = 0;
+        element.style.bototm = 0;
     }
 }loadPage();
